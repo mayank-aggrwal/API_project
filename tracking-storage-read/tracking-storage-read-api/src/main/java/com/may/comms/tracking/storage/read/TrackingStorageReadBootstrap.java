dc06fe0e8105d1e79e74b7a521cad6939adfa0e7
Mayank <aggarwal.mayank49@gmail.com>
2020-09-28 00:38:20 +0530

    Intern project init commit

---
+++ tracking-storage-read/tracking-storage-read-api/src/main/java/com/may/comms/tracking/storage/read/TrackingStorageReadBootstrap.java
@@ -0,0 +1,16 @@
+package com.may.comms.tracking.storage.read;
+
+import io.vertx.core.Future;
+import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonObject;
+
+
+public class TrackingStorageReadBootstrap {
+    public TrackingStorageReadBootstrap(final JsonObject configuration, final Vertx vertx) {
+
+    }
+
+    public void start(Future<Void> bootstrapFuture) {
+
+    }
+}
---
+++ tracking-storage-read/tracking-storage-read-api/src/main/java/com/may/comms/tracking/storage/read/TrackingStorageReadService.java
@@ -0,0 +1,30 @@
+package com.may.comms.tracking.storage.read;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class TrackingStorageReadService extends AbstractVerticle {
+
+    private static Logger LOGGER = LogManager.getLogger(TrackingStorageReadService.class);
+    private TrackingStorageReadBootstrap trackingStorageReadBootstrap;
+
+    @Override
+    public void start(final Future<Void> startFuture) {
+        LOGGER.info("Deploying Tracking storage read service");
+        trackingStorageReadBootstrap = new TrackingStorageReadBootstrap(this.config(), this.vertx);
+        final Future<Void> bootstrapFuture = Future.future();
+        trackingStorageReadBootstrap.start(bootstrapFuture);
+        bootstrapFuture.setHandler(result -> {
+            if(result.succeeded()) {
+                LOGGER.info("Succesfully deployed tracking storge servicce");
+            } else {
+                LOGGER.error("Deployment of tracking storage read service failed with cause: {}",
+                        result.cause().getMessage());
+                startFuture.fail(result.cause());
+            }
+        });
+    }
+
+}
