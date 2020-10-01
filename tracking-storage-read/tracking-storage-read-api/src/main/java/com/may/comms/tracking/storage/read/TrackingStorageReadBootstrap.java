67dc947a909b37e48368a41c30e5985d9ffc08d5
sanjeev-hash <sanjeevaggarwal770@gmail.com>
2020-10-02 00:55:44 +0530

    Add routes, and setup configuration files

--- tracking-storage-read/tracking-storage-read-api/src/main/java/com/may/comms/tracking/storage/read/TrackingStorageReadBootstrap.java
+++ tracking-storage-read/tracking-storage-read-api/src/main/java/com/may/comms/tracking/storage/read/TrackingStorageReadBootstrap.java
@@ -4,13 +4,41 @@ import io.vertx.core.Future;
 import io.vertx.core.Vertx;
 import io.vertx.core.json.JsonObject;
 
-
 public class TrackingStorageReadBootstrap {
-    public TrackingStorageReadBootstrap(final JsonObject configuration, final Vertx vertx) {
+	private final JsonObject config;
+	private final Vertx vertx;
+	private final TrackingHandler trackingHandler;
+	private final Router trackingStorageReadRouter;
 
-    }
+	public TrackingStorageReadBootstrap (final JsonObject config, final Vertx vertx) {
+		this.config = config;
+		this.vertx = vertx;
+		this.trackingStorageReadRouter = Router.router(this.vertx);
+		this.vertx.eventBus().registerCOdec(new DefaultObjectCodec());
+		this.trackingHandler = new TrackingHandlerImpl(this.vertx.eventBus());
+	}
 
-    public void start(Future<Void> bootstrapFuture) {
+	public void start(final Future<Void> bootstrapFuture) {
+		LOGGER.info("Deploying tracking storage read service");
+		final Future<Void> verticleDeploymentFuture = Future.future();
+		this.deployWorkerVerticles(verticleDeploymentFuture);
 
-    }
+		verticleDeploymentFuture.compose(futureResult -> {
+			this.initialiseRouter();
+			this.mountSubRoutes();
+		}).setHandler(handler -> {
+			if(handler.succeeded()) {
+				bootstrapFuture.complete();
+			}
+			else {
+				bootstrapFuture.fail(handler.cause());
+			}
+		})
+	}
+
+	public mountSubRoutes() {
+		this.trackingStorageReadRouter.post('events').handler(this.trackingHandler::createEvent);
+		this.trackingStorageReadRouter.post('events/update').handler(this.trackingHandler::modifyEvent);
+	}
+
 }
